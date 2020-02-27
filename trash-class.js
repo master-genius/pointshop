e14a67a1ea3c3426bf816e5ad9d282e492bee625
BraveWang <1146040444@qq.com>
2020-02-27 22:00:05 +0800

    --

---
+++ trash-class.js
@@ -0,0 +1,58 @@
+module.exports = {
+  '金属@jinshu' : {
+    '罐头@guantou' : [0.5, 50],
+    '金属餐具@jinshucanju' : [0.5, 50],
+    '平底锅@pingdiguo' : [0.5, 50],
+    '奶粉罐@naifenguan' : [0.5, 50],
+    '铁质置物架@tiezhizhiwujia' : [0.5, 50],
+    '保温杯@baowenbei' : [0.5, 50],
+    '水龙头@shuilongtou' : [0.5, 50],
+    '电线@dianxian' : [0.5, 50]
+  },
+
+  '纸类@zhilei' : {
+    '废报纸@feibaozhi' : [0.6, 60],
+    '信封@xinfeng' : [0.6, 60],
+    '纸箱@zhixiang' : [0.4, 40],
+    '杂志@zazhi' : [0.4, 40],
+    '纸袋@zhidai' : [0.4, 40],
+    '办公废纸@baogongfeizhi' : [0.4, 40],
+    '日历@rili' : [0.4, 40],
+    '废旧书籍@feijiushuji' : [0.6, 60],
+    '宣传单@xuanchuandan' : [0.4, 40]
+  },
+
+  '编织类@bianzhilei' : {
+    '毛衣@maoyi' : [0.7, 70],
+    '书包@shubao' : [0.7, 70],
+    '毛绒玩具@maorongwanju' : [0.7, 70],
+    '衣服@yifu' : [0.7, 70],
+    '鞋子@xiezi' : [0.7, 70],
+    '帽子@maozi' : [0.7, 70],
+    '窗帘@chuanglian' : [0.7, 70],
+    '浴巾毛巾@yujinmaojin' : [0.7, 70]
+  },
+
+  '塑料类@suliaolei' : {
+    '塑料水桶@suliaoshuitong' : [0.75, 75],
+    '塑料餐具@suliaocanju' : [0.75, 75],
+    '塑料玩具@' : [0.75, 75],
+    '洗漱品@xishupin' : [0.75, 75],
+    '键盘@jianpan' : [0.75, 75],
+    '塑料壳@suliaoke' : [0.75, 75],
+    '收纳箱@shounaxiang' : [0.75, 75],
+    '塑料水管@suliaoshuiguan' : [0.75, 75],
+    '塑料拖鞋@suliaotuoxie' : [0.75, 75],
+    '塑料水杯@suliaoshuibei' : [0.75, 75],
+    '塑料袋@suliaodai' : [0.75, 75],
+  },
+
+  /* '饮料瓶' : {
+    '饮料瓶' : []
+  },
+
+  '玻璃' : {
+    '玻璃' : []
+  } */
+
+};
