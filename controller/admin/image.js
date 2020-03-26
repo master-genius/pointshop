a6c06b06e1f1613ef7d3d10102d61e537a623ad8
BraveWang <1146040444@qq.com>
2020-03-27 01:01:30 +0800

    --

--- controller/admin/image.js
+++ controller/admin/image.js
@@ -1,6 +1,33 @@
+const fs = require('fs');
+
 class image {
   constructor () {
+    this.imageMap = {};
+    this.images = [];
+    this.imageCache = false;
+  }
 
+  getImageList (imgpath) {
+    try {
+      this.images = fs.readdirSync(imgpath);
+      for (let i=0; i<images.length; i++) {
+        this.imageMap[ images[i] ] = images[i];
+      }
+      this.imageCache = true;
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  async list (c) {
+    if (!this.imageCache) {
+      this.getImageList(c.service.imagepath);
+    }
+
+    c.res.body = {
+      status : 'OK',
+      data : this.imageList
+    };
   }
 
   /**
@@ -13,8 +40,11 @@ class image {
       let r = await c.moveFile(c.getFile('image'), {
         path : c.service.imagepath
       });
+      
+      this.images.unshift(r.filename);
+      this.imageMap[r.filename] = r.filename;
 
-      c. res.body = {
+      c.res.body = {
         status : 'OK',
         data : r.filename
       };
@@ -27,7 +57,25 @@ class image {
   }
 
   async delete (c) {
-    
+    try {
+      fs.unlinkSync(c.service.imagepath + '/' + c.param.name);
+      if (this.imageCache) {
+        if (this.imageMap[c.param.name] !== undefined) {
+          let ind = this.images.indexOf(c.param.name);
+          this.images.splice(ind,1);
+          delete this.imageMap[c.param.name];
+        }
+      }
+      c.res.body = {
+        status : 'OK',
+        data : 'success'
+      };
+    } catch (err) {
+      c.res.body = {
+        status : 'FAILED',
+        errmsg : err.message
+      }
+    }
   }
 
   __mid () {
